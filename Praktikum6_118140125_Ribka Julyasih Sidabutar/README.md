e8b6929f854b530e0a936d519c7ccf7b0a6d365c
ribkajsidabutar <65091705+ribkajsidabutar@users.noreply.github.com>
2020-12-17 02:05:21 +0700

    Add files via upload

---
+++ Praktikum6_118140125_Ribka Julyasih Sidabutar/README.md
@@ -0,0 +1,17 @@
+# 118140016
+
+repo praktikum Web ITERA
+
+Cara insert data:
+
+- isi form input data
+- klik tambah
+
+Cara delete data:
+
+- pada baris yang ingin dihapus, klik hapus
+
+Cara update data:
+
+- isi form input data
+- klik edit pada baris yang ingin diedit maka data akan terupdate kecuali nim
---
+++ Praktikum6_118140125_Ribka Julyasih Sidabutar/mahasiswa.sql
@@ -0,0 +1,49 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 16, 2020 at 06:45 PM
+-- Server version: 10.4.11-MariaDB
+-- PHP Version: 7.4.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `prakweb6`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `mahasiswa`
+--
+
+CREATE TABLE `mahasiswa` (
+  `nim` char(15) NOT NULL,
+  `nama` varchar(50) NOT NULL,
+  `prodi` varchar(50) NOT NULL,
+  `angkatan` char(4) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `mahasiswa`
+--
+
+INSERT INTO `mahasiswa` (`nim`, `nama`, `prodi`, `angkatan`) VALUES
+('118140125', 'Aji Saputra', 'IF', '2017'),
+('118140125', 'Ribka Julyasih Sidabutar', 'IF', '2018'),
+('18140125', 'Brina Sihite', 'TG', '2020');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
